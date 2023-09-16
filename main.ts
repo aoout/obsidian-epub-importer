c906c570abb6babf3f97e3a52ab95873859eb321
aoout <wuz66280@gmail.com>
2023-09-16 20:02:53 +0800

    Simplify the code

--- main.ts
+++ main.ts
@@ -2,13 +2,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Plugin, htmlToMarkdown } from "obsidian";
-import EpubParser from "src/epubParser";
+import { TocItem, EpubParser } from "src/epubParser";
 import { EpubModal } from "src/modal";
 
 export default class MyPlugin extends Plugin {
 	parser: EpubParser;
 	async onload() {
-		console.log("Helloworld!!");
 		this.addCommand({
 			id: "import-epub",
 			name: "Import epub to your vault",
@@ -21,61 +20,42 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async import(epubPath: string) {
-		this.parser = new EpubParser(epubPath);
-		await this.parser.init();
-		this.parser.findOpfFile();
-		this.parser.parse();
-		await new Promise((resolve) => setTimeout(resolve, 2000));
+		this.parser = await EpubParser.getParser(epubPath);
+
 		const toc = this.parser.toc;
 		const path = require("path");
 		const epubName = path.basename(epubPath, path.extname(epubPath));
 		this.app.vault.createFolder(epubName);
-		// create folder recursively according to toc
-		const create = (toc: any, parentPath: string) => {
-			// read file from path
-			const fs = require("fs");
-			if (toc.children.length >= 2) {
+		let tocContent = "";
+		tocContent += `# ${epubName}\n\n`;
+
+		const create = (toc: TocItem, parentPath: string) => {
+			this.app.vault.create(
+				parentPath + ".md",
+				htmlToMarkdown(toc.getChapter())
+			);
+
+			const filename = parentPath.split("\\").pop();
+			const level = parentPath.split("\\").length - 2;
+			tocContent += `${"\t".repeat(level)}- [[${parentPath.replace(
+				/\\/g,
+				"/"
+			)}|${filename}]]\n`;
+
+			if (toc.subItems.length >= 1) {
 				this.app.vault.createFolder(parentPath);
-				toc.children.forEach((element: any) => {
-					create(element, path.join(parentPath, element.label));
+
+				toc.subItems.forEach((element: TocItem) => {
+					create(element, path.join(parentPath, element.name));
 				});
-				const contentPath= toc.content.split('#')[0];
-				const content = fs.readFileSync(contentPath,'utf-8');
-				const markdown = htmlToMarkdown(content);
-				this.app.vault.create(
-					parentPath + ".md",
-					markdown
-				);
-			}else if(toc.children.length == 1){
-
-				const contentPath= toc.content.split('#')[0];
-				const content = fs.readFileSync(contentPath,'utf-8');
-				const markdown = htmlToMarkdown(content);
-				const contentPath2 = toc.children[0].content.split('#')[0];
-				const content2 = fs.readFileSync(contentPath2,'utf-8');
-				const markdown2 = htmlToMarkdown(content2);
-
-				this.app.vault.create(
-					parentPath + ".md",
-					markdown + markdown2
-				);
-			}else{
-				const contentPath= toc.content.split('#')[0];
-				const content = fs.readFileSync(contentPath,'utf-8');
-				const markdown = htmlToMarkdown(content);
-				this.app.vault.create(
-					parentPath + ".md",
-					markdown
-				);
 			}
-			
-
-			
 		};
 
 		for (let i = 0; i < toc.length; i++) {
-			create(toc[i], path.join(epubName, toc[i].label));
+			create(toc[i], path.join(epubName, toc[i].name));
 		}
+
+		this.app.vault.create(epubName + "//" + "toc.md", tocContent);
 	}
 
 	onunload() {}
--- src/epubParser.ts
+++ src/epubParser.ts
@@ -1,47 +1,51 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-var-requires */
-export default class EpubParser {
+
+export class TocItem {
+	name: string;
+	url: string;
+	subItems: TocItem[];
+
+	constructor(
+		name: string,
+		url: string,
+		subItems: TocItem[] = new Array<TocItem>()
+	) {
+		this.name = name;
+		this.url = url;
+		this.subItems = subItems;
+	}
+
+	getChapter(): string {
+		const fs = require("fs");
+		const chapter = fs.readFileSync(this.url.split("#")[0], "utf-8");
+		return chapter;
+	}
+}
+
+export class EpubParser {
 	epubPath: string;
 	tmpobj: any;
 	tocFile: string;
-	toc: any;
+	toc: TocItem[];
 
 	constructor(path: string) {
 		this.epubPath = path;
 	}
 
 	async init() {
-		// create a temp directory
 		const tmp = require("tmp");
 		this.tmpobj = tmp.dirSync({ unsafeCleanup: true });
-		console.log("Dir: ", this.tmpobj.name);
-		// unzip the epub file to the temp directory Synchronize
-		await this.unzipSync();
-	}
-
-	unzipSync() {
 		const fs = require("fs");
 		const unzipper = require("unzipper");
-		return new Promise<void>((resolve, reject) => {
-			fs.createReadStream(this.epubPath)
-				.pipe(
-					unzipper.Extract({
-						path: this.tmpobj.name,
-					})
-				)
-				.on("close", () => {
-					console.log("stream close");
-					resolve();
-				})
-				.on("error", (err: any) => {
-					reject(err);
-				});
-		});
+		await fs
+			.createReadStream(this.epubPath)
+			.pipe(unzipper.Extract({ path: this.tmpobj.name }))
+			.promise();
 	}
 
 	findOpfFile() {
-		// recursively traverse the folder and find the path of the file named content.opf, get it's path
 		const path = require("path");
 		const fs = require("fs");
 		const walkSync = (currentDirPath: string, callback: any) => {
@@ -61,60 +65,53 @@ export default class EpubParser {
 			}
 		});
 		console.log("toc file path: ", this.tocFile);
-
 	}
 
-	parse() {
-		// read opf file and parse it
+	async parse() {
 		const fs = require("fs");
 		const xml2js = require("xml2js");
 		const parser = new xml2js.Parser();
-		fs.readFile(this.tocFile, (err: any, data: any) => {
-			parser.parseString(data, (err: any, result: any) => {
-				console.log(result);
-				const navPoints = result.ncx.navMap[0].navPoint;
-				console.log(navPoints);
-				const toc = [];
-				const parseNavPoint = (navPoint: any) => {
+		const data = fs.readFileSync(this.tocFile, "utf-8");
 
-					interface NavPointObj {
-						label: string;
-						content: string;
-						children: NavPointObj[];
-					}
-					const navPointObj: NavPointObj = {
-						label: navPoint.navLabel[0].text[0],
-						content:
-							this.tmpobj.name +
-							"/" +
-							navPoint.content[0].$["src"],
-						children: [],
-					};
-					if (navPoint["navPoint"] && navPoint["navPoint"].length > 1 ) {
-						if(navPoint["navPoint"][1].content[0].$["src"].indexOf('#') != -1){
-							navPointObj.children.push(parseNavPoint(navPoint["navPoint"][0]));
-						}else{
-							for (
-								let i = 0;
-								i < navPoint["navPoint"].length;
-								i++
-							) {
-								const child = navPoint["navPoint"][i];
-								navPointObj.children.push(parseNavPoint(child));
-							}
-						}
-						
-					}
-					return navPointObj;
-				};
-				for (let i = 0; i < navPoints.length; i++) {
-					const item = navPoints[i];
-					toc.push(parseNavPoint(item));
-				}
-				this.toc = toc;
-				console.log(toc);
-			});
+		let result: any;
+		await parser.parseStringPromise(data).then((result2: any) => {
+			result = result2;
 		});
+		console.log(result);
+		const navPoints = result.ncx.navMap[0].navPoint;
 
+		const parseNavPoint = (navPoint: any) => {
+			const path = require("path");
+			const tocParentPath = path.dirname(this.tocFile);
+
+			const item = new TocItem(
+				navPoint.navLabel[0].text[0],
+				tocParentPath + "/" + navPoint.content[0].$["src"]
+			);
+
+			const subNavPoints = navPoint["navPoint"];
+			if (subNavPoints) {
+				for (let i = 0; i < subNavPoints.length; i++) {
+					const child = subNavPoints[i];
+					item.subItems.push(parseNavPoint(child));
+				}
+			}
+			return item;
+		};
+		const toc = [];
+		for (let i = 0; i < navPoints.length; i++) {
+			const item = navPoints[i];
+			toc.push(parseNavPoint(item));
+		}
+		this.toc = toc;
+		console.log(toc);
+	}
+
+	static async getParser(path: string) {
+		const parser = new EpubParser(path);
+		await parser.init();
+		parser.findOpfFile();
+		await parser.parse();
+		return parser;
 	}
 }
