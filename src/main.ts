c3eb36aa8806e4226fd3105ae9272ca38dd85320
aoout <wuz66280@gmail.com>
2023-12-06 18:37:59 +0800

    No longer breaks when  no cover information.

--- src/main.ts
+++ src/main.ts
@@ -156,10 +156,13 @@ export default class EpubImporterPlugin extends Plugin {
 		).forEach((file)=>{
 			jetpack.copy(file,imagesPath.join(new Path(file).name).string,{overwrite:true});
 		});
-		this.propertys.cover = new Path(
-			this.assetsPath,
-			new Path(this.parser.coverPath).name
-		).string;
+		if(this.parser.coverPath){
+			this.propertys.cover = new Path(
+				this.assetsPath,
+				new Path(this.parser.coverPath).name
+			).string;
+		}
+		
 	}
 
 	async Chapter2MD(epubName: string, cpt: Chapter, notePath: Path, serialNumber:number[]) {
