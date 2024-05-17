5fbe2835a4e3698e92a6544e03ab531b62454352
aoout <wuz66280@gmail.com>
2024-05-17 15:16:58 +0800

    refactor:  extracted BaseModal

--- src/modal.ts
+++ src/modal.ts
@@ -1,16 +1,24 @@
-import EpubImporterPlugin from "./main";
-import { App, Notice, SuggestModal } from "obsidian";
+import { App } from "obsidian";
 import jetpack from "fs-jetpack";
+import { BaseModal } from "./utils/baseModal";
 
-export class EpubImporterModal extends SuggestModal<string> {
-	onSubmit: (result: string) => void;
+export class EpubImporterModal extends BaseModal {
 	libraries: string[];
-	emptyStateText = "No .epub files found in libraries.";
+
+	constructor(app: App, libraries: string[], onSubmit: (result: string) => void) {
+		super(app, onSubmit, true);
+		this.libraries = libraries;
+		this.listenForEnter(this.validatePath);
+	}
+
+	validatePath(path: string): boolean {
+		return jetpack.exists(path) === "file" && path.endsWith(".epub");
+	}
 
 	getSuggestions(query: string): string[] | Promise<string[]> {
 		const result: string[] = [];
 		this.libraries.forEach((lib) => {
-			jetpack.find(lib, { matching: "**/**.epub" }).forEach((path) => {
+			jetpack.find(lib, { matching: "**/*.epub" }).forEach((path) => {
 				result.push(jetpack.path(path));
 			});
 		});
@@ -22,44 +30,6 @@ export class EpubImporterModal extends SuggestModal<string> {
 	}
 
 	onChooseSuggestion(item: string) {
-		this.trySubmit(item);
-	}
-
-	trySubmit(path: string) {
-		const epubPath = path.replace(/^"(.+(?="$))"$/, "$1");
-		try {
-			this.onSubmit(epubPath);
-			this.close();
-		} catch (error) {
-			new Notice("Invalid path.");
-		}
-	}
-
-	listenfromFile() {
-		this.inputEl.addEventListener("paste", (data) => {
-			if (data.clipboardData.files.length == 1) {
-				//@ts-ignore
-				const path = data.clipboardData.files[0].path;
-				this.inputEl.value = path;
-			}
-		});
-	}
-
-	listenfromEnter(func: () => void) {
-		this.inputEl.addEventListener("keyup", ({ key }) => {
-			if (key === "Enter") {
-				func();
-			}
-		});
-	}
-
-	constructor(app: App, plugin: EpubImporterPlugin, onSubmit: (result: string) => void) {
-		super(app);
-		this.onSubmit = onSubmit;
-		this.libraries = plugin.settings.libraries;
-		this.listenfromFile();
-		this.listenfromEnter(() => {
-			if (this.inputEl.value) this.trySubmit(this.inputEl.value);
-		});
+		this.trySubmit(item, this.validatePath);
 	}
 }
---
+++ src/utils/BaseModal.ts
@@ -0,0 +1,39 @@
+import { SuggestModal, Notice, App } from "obsidian";
+
+export abstract class BaseModal extends SuggestModal<string> {
+	constructor(app: App, private onSubmit: (result: string) => void, enablePaste = false) {
+		super(app);
+		this.onSubmit = onSubmit;
+		if (enablePaste) {
+			this.listenForPaste();
+		}
+	}
+
+	listenForPaste() {
+		this.inputEl.addEventListener("paste", (event) => {
+			const clipboardData = event.clipboardData;
+			if (clipboardData.files.length === 1) {
+				//@ts-ignore
+				const path = clipboardData.files[0].path;
+				this.inputEl.value = path;
+			}
+		});
+	}
+
+	listenForEnter(validate: (value: string) => boolean) {
+		this.inputEl.addEventListener("keyup", (event) => {
+			if (event.key === "Enter" && this.inputEl.value) {
+				this.trySubmit(this.inputEl.value, validate);
+			}
+		});
+	}
+
+	trySubmit(value: string, validate: (value: string) => boolean) {
+		if (validate(value)) {
+			this.onSubmit(value);
+			this.close();
+		} else {
+			new Notice("Invalid value.");
+		}
+	}
+}
